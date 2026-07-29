a655afee8905ae96925d50db7ea5cd36cd166dcc
loki-isl <lokeshkumar.dhanapalan@inspirisys.com>
2026-07-29 17:27:52 +0530

    Initial portfolio commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/loki-portfolio/',  
+  base: '/loki-portfolio-v2/',  
 })
